0470db3d41fc17b2c8268c659bcd330217719f83
samirvithlani <samir.vithlani83955@gmail.com>
2023-05-17 11:13:45 +0530

    add

---
+++ basic2/basic1.py
@@ -0,0 +1,25 @@
+#printf("")
+print("hello")
+
+# a = 10
+# b = 3
+a = int(input("enter value of a :-"))
+b = int(input("enter value of b :-"))
+c =0
+
+print("value of a :-",a)
+print("value of b :-",b)
+
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+p = a **b
+#floor division
+q = a // b
+print("value of c =",c)
+print("value of d =",d)
+print("value of e =",e)
+print("value of f =",f)
+print("value of p =",p)
+print("value of q =",q)
