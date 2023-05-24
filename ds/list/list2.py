6b5c2c0a101ea6c4e2fa29f654a85c51d73181d8
samirvithlani <samir.vithlani83955@gmail.com>
2023-05-24 10:01:52 +0530

    list

---
+++ ds/list/list2.py
@@ -0,0 +1,34 @@
+users = ["raj","ram","parth"]
+#list is mutable : after declaration we can add or remove elements
+users.append("ravi")
+users.insert(1,"meet")
+
+users.extend(["manan","mehul","mohit"])
+
+print(users)
+
+#pop is overloded function
+# rmvelm = users.pop()
+# removedelm = users.pop(3)
+# print("removed element is ",removedelm)
+
+# cnt =users.count("ravii")
+# print("count is ",cnt)
+
+# users.remove("ravi")
+
+#users.sort()
+#users.sort(reverse=True)
+
+#users.clear()
+#print(users)
+
+#susers.reverse()
+# usercopy = users.copy()
+# print(usercopy)
+
+# usercopy.append("royal")
+
+for i in users:
+    print(i)
+    
