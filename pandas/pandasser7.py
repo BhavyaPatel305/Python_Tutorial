7a9f59dc2198b95e8f7eb635b5d7bc54b77166ba
samirvithlani <samir.vithlani83955@gmail.com>
2023-07-14 10:10:43 +0530

    pandas series

---
+++ pandas/pandasser7.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = pd.read_csv("./ipl.csv")
+
+arr = pd.unique(data["Team"])
+print(arr)
+arr1 = data["runs"]
+bool_ser = arr1.between(50,100,inclusive ="both")
+print(bool_ser)
